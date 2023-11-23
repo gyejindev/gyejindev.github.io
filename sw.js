4c17b47322b524b002bdc71097853cd46981b8d8
gyejindev <108448165+gyejindev@users.noreply.github.com>
2023-11-23 18:43:47 +0900

    202311231840
    202311231840

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231123.1733';
+const cacheName = 'chirpy-20231123.1743';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
