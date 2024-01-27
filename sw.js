5bfd5f64f9843acba402ecac68609de298d25960
gyejindev <108448165+gyejindev@users.noreply.github.com>
2024-01-27 23:00:50 +0900

    git사용법 추가
    git사용법 추가

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240124.0018';
+const cacheName = 'chirpy-20240127.2200';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
