93da787df4a3ed6a771a80d230b48b0ea017f18e
gyejin.dev <gyejin.dev@gmail.com>
2024-05-10 23:42:01 +0900

    수정

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240418.2230';
+const cacheName = 'chirpy-20240510.2241';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
