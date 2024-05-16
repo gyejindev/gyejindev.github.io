a55cc816b248ec73ee6631a9b893d87dabcc958c
gyejin.dev <gyejin.dev@gmail.com>
2024-05-17 00:29:03 +0900

    202405161228

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240516.1348';
+const cacheName = 'chirpy-20240516.2325';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
